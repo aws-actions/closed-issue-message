1d745d6caf6cedf9ab715cdb3daf22d41a17ee9a
Tom Keller <kellertk@amazon.com>
2025-01-30 14:33:10 -0800

    omg

--- entrypoint.js
+++ entrypoint.js
@@ -5,6 +5,10 @@ import { exit } from 'node:process';
 const token = core.getInput('repo-token', { required: true });
 const octokit = github.getOctokit(token);
 const message = core.getInput('message', { required: true });
+console.debug(`GitHub: ${JSON.stringify(github)}`);
+console.debug(`Octokit: ${JSON.stringify(octokit)}`);
+console.debug(`Message: ${message}`);
+console.debug(`Context: ${JSON.stringify(github.context)}`);
 
 const issueNumber = github.context.payload.issue?.number || github.context.payload.pull_request?.number;
 if (!issueNumber) {
@@ -14,7 +18,7 @@ if (!issueNumber) {
 
 try {
   await octokit.rest.issues.createComment({
-    ...context.repo,
+    ...github.context.repo,
     issue_number: issueNumber,
     body: message
   });
